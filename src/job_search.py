1889010f877dbdb1aa250a02da9c8540677ea060
Dmitry Dronov <cskafan1911@gmail.com>
2023-09-15 20:27:56 +0300

    Создан файл vacancy.py, c классом Vacancy.

--- src/job_search.py
+++ src/job_search.py
@@ -2,13 +2,22 @@ from abc import ABC
 
 
 class JobSearchAPI(ABC):
+    """
+    Абстрактный класс для работы с API сайтов с вакансиями.
+    """
     pass
 
 
 class HeadHunterAPI(JobSearchAPI):
+    """
+    Класс для работы с API сайта HeadHunter.
+    """
     pass
 
 
 class SuperJobAPI(JobSearchAPI):
+    """
+    Класс для работы с API сайта SuperJob.
+    """
     pass
 
---
+++ src/vacancy.py
@@ -0,0 +1,3 @@
+class Vacancy:
+    pass
+
