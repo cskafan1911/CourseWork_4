94cb4f55f5ceb13bba468994f1a2cb6e542bfefd
Dmitry Dronov <cskafan1911@gmail.com>
2023-09-17 16:08:32 +0300

    В абстрактном классе JobSearcnAPI добавлен метод format_vacancies.

--- src/job_search.py
+++ src/job_search.py
@@ -1,18 +1,64 @@
-from abc import ABC
+import json
+from abc import ABC, abstractmethod
+
+import requests
 
 
 class JobSearchAPI(ABC):
     """
     Абстрактный класс для работы с API сайтов с вакансиями.
     """
-    pass
+
+    @abstractmethod
+    def get_vacancies(self, job_title):
+        """
+        Метод возвращает информацию о вакансии по ключевому слову
+        """
+        pass
+
+    @abstractmethod
+    def format_vacancies(self, vacancies_data):
+        """
+        Метод получает данные о вакансиях и приводит к нужному формату
+        :param vacancies_data: Информация о вакансиях
+        :return: отформатированный список вакансий
+        """
+        pass
 
 
 class HeadHunterAPI(JobSearchAPI):
     """
     Класс для работы с API сайта HeadHunter.
     """
-    pass
+    URL = "https://api.hh.ru/vacancies"
+
+    def __init__(self):
+        """
+        Инициализатор класса HeadHunterAPI
+        """
+        self.vacancies = None
+        self.job_title = None
+
+    def get_vacancies(self, job_title):
+        """
+        Метод сохраняет список вакансий полученный по API
+        """
+        self.job_title = job_title
+        params = {'text': job_title, "per_page": 50}
+        response = requests.get(self.URL, params, verify=False)
+        data = response.content.decode(encoding='utf-8')
+        self.vacancies = json.loads(data)
+
+        return self.vacancies
+
+    def format_vacancies(self, vacancies_data):
+        """
+        Метод получает данные о вакансиях и приводит к нужному формату
+        :param vacancies_data: Информация о вакансиях
+        :return: отформатированный список вакансий
+        """
+        pass
+
 
 
 class SuperJobAPI(JobSearchAPI):
@@ -20,4 +66,3 @@ class SuperJobAPI(JobSearchAPI):
     Класс для работы с API сайта SuperJob.
     """
     pass
-
