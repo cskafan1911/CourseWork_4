ebd1e9ef4e330a7040a8e24eb9fa0e1fd93d52aa
Dmitry Dronov <cskafan1911@gmail.com>
2023-09-20 21:46:40 +0300

    Реализованы классы JSONManager, CSVManager. Методы write_file, read_file.

---
+++ config.py
@@ -0,0 +1,3 @@
+FILENAME = "vacancies_list"
+URL_HH = "https://api.hh.ru/vacancies"
+URL_SJ = 'https://api.superjob.ru/2.0/vacancies/'
--- src/file_manager.py
+++ src/file_manager.py
@@ -1,7 +1,8 @@
 import json
+import os
+from csv import DictReader, DictWriter
 from abc import ABC, abstractmethod
-
-from src.vacancy import Vacancy
+from config import FILENAME
 
 
 class FileManager(ABC):
@@ -17,10 +18,9 @@ class FileManager(ABC):
         pass
 
     @abstractmethod
-    def read_file(self, filename):
+    def read_file(self):
         """
         Метод для чтения файла
-        :param filename: имя файла для чтения
         """
         pass
 
@@ -29,15 +29,70 @@ class JSONManager(FileManager):
     """
     Класс для работы с файлами формата json
     """
+
+    def __init__(self):
+        self.__filename = FILENAME + ".json"
+
     def save_file(self, data):
         """
         Метод сохраняет список вакансий в файл формата JSON
         """
-        with open('files_witn_vacancies/HH_vacancies.json', 'w', encoding='utf-8') as json_file:
-            json.dump(self.verified_vacancies, json_file, ensure_ascii=False, indent=2)
+
+        if os.path.exists(self.__filename):
+            with open(self.__filename, 'r', encoding='utf-8') as file:
+                vacancy_data = json.load(file)
+        else:
+            vacancy_data = []
+
+        vacancy_data.append(data)
+
+        with open(self.__filename, 'w', encoding='utf-8') as file:
+            json.dump(vacancy_data, file, ensure_ascii=False, indent=2)
+
+    def read_file(self):
+        """
+        Метод для чтения файла
+        """
+        with open(self.__filename, 'r', encoding='utf-8') as json_file:
+            json_data = json.load(json_file)
+
+        return json_data
 
 
 class CSVManager(FileManager):
     """
     Класс для работы с файлами формата CSV
     """
+
+    def __init__(self):
+        self.__filename = FILENAME + ".csv"
+
+    def save_file(self, data):
+        """
+        Метод сохраняет данные в файл нужного формата
+        :param data: Данные для сохранения
+        """
+        reader = [value for value in data]
+        headers = [key for key in data[0].keys()]
+
+        if os.path.exists(self.__filename):
+            with open(self.__filename, 'a',  encoding='utf-8-sig') as file:
+                writer = DictWriter(file, fieldnames=headers)
+                for line in reader:
+                    writer.writerow(line)
+
+        else:
+            with open(self.__filename, 'w', encoding='utf-8-sig') as file:
+                writer = DictWriter(file, fieldnames=headers)
+                writer.writeheader()
+                for line in reader:
+                    writer.writerow(line)
+
+    def read_file(self):
+        """
+        Метод для чтения файла
+        """
+        with open(self.__filename, 'r', encoding='utf-8-sig') as file:
+            reader = DictReader(file)
+
+        return reader
