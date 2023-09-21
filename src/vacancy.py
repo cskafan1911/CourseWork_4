1cba1cee2e565aeae67240dceb8340ba4e1637df
Dmitry Dronov <cskafan1911@gmail.com>
2023-09-21 23:49:59 +0300

    Написан инициализатор класса Vacancy. Написан метод format_salary.

--- src/vacancy.py
+++ src/vacancy.py
@@ -2,45 +2,49 @@ class Vacancy:
     """
     Класс для работы с вакансиями
     """
-    def __init__(self, vacancies_data):
+    def __init__(self, vacancies_data: dict):
         """
         Инициализация класса Vacancy
         :param vacancies_data: данные о вакансиях
         """
-        self.validate_data = self.format_salary(vacancies_data)
+        self.__vacancy = vacancies_data
+        self.__vacancy_name = self.__vacancy['Вакансия']
+        self.__city = self.__vacancy['Город']
+        self.__url = self.__vacancy['Ссылка']
+        self.__salary = self.format_salary(self.__vacancy['Зарплата'])
 
     @staticmethod
-    def format_salary(data):
+    def format_salary(salary):
         """
-        Метод для обработки данных по зарплате в нужный формат
-        :param data: Список вакансий
-        :return: Список вакансий с отформатированной зарплатой
+        Метод для обработки данных по зарплате
+        :param salary: данные по зарплате
+        :return: отредактированная зарплата
         """
-        for data_list in data:
-            for salary in data_list:
-                if salary['Зарплата'] is None:
-                    formatted_salary = 0
-                elif isinstance(salary['Зарплата'], dict):
-                    if isinstance(salary['Зарплата']['from'], int) and not isinstance(salary['Зарплата']["to"], int):
-                        formatted_salary = salary['Зарплата']["from"]
-                    elif not isinstance(salary['Зарплата']['from'], int) and isinstance(salary['Зарплата']["to"], int):
-                        formatted_salary = salary['Зарплата']["to"]
-                    elif not isinstance(salary['Зарплата']['from'], int) and not isinstance(salary['Зарплата']["to"],
-                                                                                            int):
-                        formatted_salary = 0
-                    else:
-                        formatted_salary = max(salary['Зарплата']['to'], salary['Зарплата']["from"])
+        if salary is None:
+            formatted_salary = 0
+        elif isinstance(salary, dict):
+            if isinstance(salary['from'], int) and not isinstance(salary["to"], int):
+                formatted_salary = salary["from"]
+            elif not isinstance(salary['from'], int) and isinstance(salary["to"], int):
+                formatted_salary = salary["to"]
+            elif not isinstance(salary['from'], int) and not isinstance(salary["to"], int):
+                formatted_salary = 0
+            else:
+                formatted_salary = max(salary['to'], salary["from"])
 
-                    if salary['Зарплата']['currency'] == 'USD':
-                        formatted_salary *= 96.65
-                    elif salary['Зарплата']['currency'] == 'EUR':
-                        formatted_salary *= 103.09
-                    elif salary['Зарплата']['currency'] == 'BYR':
-                        formatted_salary *= 29.59
+            if salary['currency'] == 'USD':
+                formatted_salary *= 96.65
+            elif salary['currency'] == 'EUR':
+                formatted_salary *= 103.09
+            elif salary['currency'] == 'BYR':
+                formatted_salary *= 29.59
 
-                elif isinstance(salary, list):
-                    formatted_salary = max(salary['Зарплата'])
+        else:
+            formatted_salary = max(salary)
 
-                salary['Зарплата'] = round(formatted_salary)
+        return round(formatted_salary)
 
-        return data
+    @property
+    def salary(self):
+
+        return self.__salary
